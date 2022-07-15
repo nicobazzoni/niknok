12e077b91107de7e74ff77feba609c7ff6923b79
nico bazzoni <nicobazzoni@gmail.com>
2022-07-15 16:39:25 -0400

    first commit

---
+++ components/NoResults.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+interface IProps {
+    text: string;
+}
+const NoResults = ({ text }: IProps) => {
+    return (
+        <div>NoResults</div>
+    )
+}
+
+export default NoResults
