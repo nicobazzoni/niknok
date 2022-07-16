9813fdfcaea10b698896b16737d2899883c089d1
nico bazzoni <nicobazzoni@gmail.com>
2022-07-15 20:51:16 -0400

    css change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   content: [
-    '.apps/site/pages/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    
   ],
   theme: {
     extend: {
